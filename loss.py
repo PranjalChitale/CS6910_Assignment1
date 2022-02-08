9d2eb315b892224b201fe20bce26a262b3e28e7b
Pranjal Chitale <62383456+PranjalChitale@users.noreply.github.com>
2022-02-09 01:33:36 +0530

    Update loss.py

--- loss.py
+++ loss.py
@@ -1,6 +1,6 @@
 def cross_entropy(y_true, y_hat):
     """
-    Assumes, y is one-hot vector.
+    Assumes, y_true and y_hat are one-hot vectors.
     """
     true_label = np.argmax(y_true)
     pred_label = np.argmax(y_hat)
@@ -9,5 +9,8 @@ def cross_entropy(y_true, y_hat):
     return loss
 
 def squared_error(y_true, y_hat):
-    loss = np.square(y_hat - y_true)
+    """
+    Assumes, y_true and y_hat are one-hot vectors.
+    """
+    loss = np.mean(np.square(y_hat - y_true))
     return loss
